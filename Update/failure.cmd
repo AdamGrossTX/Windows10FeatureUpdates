e9e2c3f1891aa8f7b1d50530846c59d9b6c501a9
Adam Gross <github@grosshome.com>
2019-08-14 09:22:24 -0500

    Typo

--- Update/failure.cmd
+++ Update/failure.cmd
@@ -1,6 +1,6 @@
 @ECHO ON
 Echo BEGIN failure.cmd >> c:\Windows\CCM\Logs\FeatureUpdate-failure.Log
 
-START Powershell.exe -ExecutionPolicy Bypass -WindowStyle -Hidden -File "C:\~FeatureUpdateTemp\Scripts\Copy-FeatureUpdateLogs.ps1"
+START Powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\~FeatureUpdateTemp\Scripts\Copy-FeatureUpdateLogs.ps1"
 
 Echo END failure.cmd >> C:\Windows\CCM\Logs\FeatureUpdate-failure.Log
